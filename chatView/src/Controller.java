dba9282922168f05837536e79c696b9b0c01206d
mars-janda <65905992+mars-janda@users.noreply.github.com>
2021-05-19 13:23:34 +0200

    Initial Commit
    Copied jfx template and renamed everything to chatView

---
+++ chatView/src/Controller.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField op2TextField;
+    public TextField op1TextField;
+    public TextField resTextField;
+
+    public void onPlusButton(ActionEvent actionEvent) {
+    }
+
+    public void onMinusButton(ActionEvent actionEvent) {
+    }
+
+    public void onTimesButton(ActionEvent actionEvent) {
+    }
+
+    public void onDivideButton(ActionEvent actionEvent) {
+    }
+}
---
+++ chatView/src/m4rsChat/Controller.java
@@ -0,0 +1,5 @@
+package m4rsChat;
+
+public class Controller {
+
+}
