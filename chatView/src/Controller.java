c684c5a4853d5d22c24fb86d75c2f2856c8dcefd
mars-janda <65905992+mars-janda@users.noreply.github.com>
2021-06-02 14:34:53 +0200

    Initial GUI
    Created a basic mock up of the gui
    2:00-2:34

--- chatView/src/Controller.java
+++ chatView/src/Controller.java
@@ -1,20 +1,22 @@
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    public TextField op2TextField;
-    public TextField op1TextField;
-    public TextField resTextField;
 
-    public void onPlusButton(ActionEvent actionEvent) {
+
+    public TextField fileName;
+    public Button openButton;
+    public MenuItem fileClose;
+    public MenuItem fileOpen;
+
+    public void fileOpen(ActionEvent actionEvent) {
     }
 
-    public void onMinusButton(ActionEvent actionEvent) {
+    public void fileClose(ActionEvent actionEvent) {
     }
 
-    public void onTimesButton(ActionEvent actionEvent) {
-    }
-
-    public void onDivideButton(ActionEvent actionEvent) {
+    public void openButton(ActionEvent actionEvent) {
     }
 }
