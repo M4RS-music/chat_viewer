3a9232fe8a2b77e9c52bc2f2029f2bb53d760ef9
mars-janda <65905992+mars-janda@users.noreply.github.com>
2021-06-02 23:13:45 +0200

    Basic structure
    10:30-11

--- chatView/src/m4rsChat/Main.java
+++ chatView/src/m4rsChat/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("chatView_m4rs");
+        primaryStage.setScene(new Scene(root, 500, 900));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
